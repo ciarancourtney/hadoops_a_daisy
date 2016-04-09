b13b3eb3f8ded0e0dc172f6e060e6543a19a41b8
ciarancourtney <ciaran.courtney@cylon.com>
2016-04-09 20:20:40 +0100

    Allow nginx to listen from any request src

--- bin/deploy_nginx.sh
+++ bin/deploy_nginx.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 echo "Installing nginx"
-sudo apt-get update
-sudo apt-get install nginx
+sudo apt-get update -yqq
+sudo apt-get install -yqq nginx
 
 echo "Adding timemap config to nginx"
+sudo rm /etc/nginx/sites-enabled/default
 sudo cp conf/timemap.conf /etc/nginx/conf.d/
 
 echo "Reloading nginx"
